97f57244624c9c280cd358337b0e5622c50845f4
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-10 17:46:22 -0400

    Delete all JS for password & confirm

--- app.js
+++ app.js
@@ -8,38 +8,9 @@ email.addEventListener("input", (event) => {
     }
 });
 
-const password = document.getElementById("password");
 
-const confirmPassword = document.getElementById("confirmPassword");
-
-function validatePassword(form) {
-    password = form.password.value;
-    confirmPassword = form.confirmPassword.value;
-
-    if (password == '')
-        alert("Please enter ur password babe");
-    else if (confirmPassword == '')
-        alert(Please confirm password babe!);
-    else if(password != confirmPassword) {
-        alert("passwords don't match, babe...")
-        return false;
-    } else {
-        alert("Passwords match - welcome!")
-        return true;
-    }
-}
-
-// confirmPassword.addEventListener("input", (event) => {
-//     if (confirmPassword === password) {
-//         confirmPassword.setCustomValidity("");
-//     } else {
-//         confirmPassword.setCustomValidity("try again")
-//     }
-// });
 
 
 window.onload = () => {
     document.getElementById("email").oninput = checkEmail;
-    document.getElementById("password").oninput = validatePassword;
-    document.getElementById("confirmPassword").oninput = validatePassword;
 }
