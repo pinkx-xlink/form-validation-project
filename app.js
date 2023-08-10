2d4fffc4c308a94e670c6c02b8b9f4286f058ea9
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-09 22:39:47 -0400

    Add customValidity to check if email = valid

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const email = document.getElementById("email");
 
+email.addEventListener("input", (event) => {
+    if(email.validity.typeMismatch) {
+        email.setCustomValidity("Please enter an EMAIL, silly!");
+    } else {
+        email.setCustomValidity("")
+    }
+});
