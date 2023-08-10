dc39d20ecd75fae57f646cf607d3a9c084b30579
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-10 17:55:11 -0400

    Change confirmPassword to confirm-password

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ email.addEventListener("input", (event) => {
     }
 });
 
-
-
+let check = function() {
+    if(document.getElementById('password').value ==
+        document.getElementById('confirm-password').value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'matching';
+    } else {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'not matching';
+    }
+}
 
 window.onload = () => {
     document.getElementById("email").oninput = checkEmail;
