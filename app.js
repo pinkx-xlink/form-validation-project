56ff13edb5c8596e7a4fb7c7ad73cf7944fd780b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-09 22:51:47 -0400

    Add defer to script src

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const email = document.getElementById("email");
 
 email.addEventListener("input", (event) => {
-    if(email.validity.typeMismatch) {
+    if (email.validity.typeMismatch) {
         email.setCustomValidity("Please enter an EMAIL, silly!");
     } else {
         email.setCustomValidity("")
